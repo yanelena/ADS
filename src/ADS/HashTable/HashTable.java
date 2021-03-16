7879c95d6d862bff849f3912faa3e1ca02adc503
yanelena <fureevaalyona@mail.ru>
2021-03-17 00:59:40 +0300

    add hashTable class

---
+++ src/ADS/HashTable/HashTable.java
@@ -0,0 +1,62 @@
+package ADS.HashTable;
+
+public class HashTable {
+    public int size;
+    public int step;
+    public String[] slots;
+
+    public HashTable(int sz, int stp) {
+        size = sz;
+        step = stp;
+        slots = new String[size];
+        for (int i = 0; i < size; i++) slots[i] = null;
+    }
+
+    public int hashFun(String value) {
+
+        // всегда возвращает корректный индекс слота
+        //остаток от деления длины строки на длину массива
+        return value.length() % this.size;
+    }
+
+    public int seekSlot(String value) {
+        if (slots[hashFun(value)] == null) {
+            // находит индекс пустого слота для значения, или -1
+            return hashFun(value);
+        } else {
+            for (int i = hashFun(value); i < this.size-1; i+=this.step) {
+                System.out.println("-_-_-_"+i);
+                if (slots[i] == null) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public int put(String value) {
+        int index=seekSlot(value);
+        if (index == -1) {
+            return -1;
+        } else {
+            slots[index] = value;
+            return hashFun(value);
+        }
+
+        // записываем значение по хэш-функции
+
+        // возвращается индекс слота или -1
+        // если из-за коллизий элемент не удаётся разместить
+
+    }
+
+    public int find(String value) {
+        int index=hashFun(value);
+        for (int i=index; i<this.size-1;i+=this.step){
+            if (slots[i]==value) return i;
+        }
+            // находит индекс слота со значением, или -1
+            return -1;
+
+    }
+}
