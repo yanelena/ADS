6a548872e5194fb5e67911c09246de1d89e5bd37
yanelena <fureevaalyona@mail.ru>
2021-04-01 00:42:12 +0300

    added all functionality

--- src/ADS/NativeDictionary/NativeDictionary.java
+++ src/ADS/NativeDictionary/NativeDictionary.java
@@ -2,40 +2,65 @@ package ADS.NativeDictionary;
 
 import java.lang.reflect.Array;
 
-class NativeDictionary<T>
-{
+class NativeDictionary<T> {
     public int size;
-    public String [] slots;
-    public T [] values;
+    public String[] slots;
+    public T[] values;
 
-    public NativeDictionary(int sz, Class clazz)
-    {
+    public NativeDictionary(int sz, Class clazz) {
         size = sz;
         slots = new String[size];
         values = (T[]) Array.newInstance(clazz, this.size);
     }
 
-    public int hashFun(String key)
-    {
+    public int hashFun(String key) {
+        int index = key.length() % this.size;
+        for (int i = 0; i < this.size; i++) {
+            if (slots[index] == null || slots[index].equals(key)) {
+                return index;
+            } else {
+                index = (index + 1) % this.size;
+            }
+        }
+        return -1;
         // всегда возвращает корректный индекс слота
-        return 0;
+//при вбивании существующего ключа, значение перезаписывается
     }
 
-    public boolean isKey(String key)
-    {
+    public boolean isKey(String key) {
+        int index = key.length() % this.size;
+        for (int i = 0; i < this.size; i++) {
+            if (slots[index] != null) {
+                if (slots[index].equals(key)) {
+                    return true;
+                } else {
+                    index = (index + 1) % this.size;
+                }
+            }
+        }
+
         // возвращает true если ключ имеется,
         // иначе false
         return false;
     }
 
-    public void put(String key, T value)
-    {
+    public void put(String key, T value) {
+        int index = hashFun(key);
+        if (index > -1) {
+            slots[index] = key;
+            Array.set(values, index, value);
+        }
         // гарантированно записываем
         // значение value по ключу key
     }
 
-    public T get(String key)
-    {
+    public T get(String key) {
+        int index = hashFun(key);
+
+        if (isKey(key) == true) {
+
+            return (T) Array.get(values, hashFun(key));
+        }
         // возвращает value для key,
         // или null если ключ не найден
         return null;
