c5da4d66f4eca13a5c339e8a7546b30f87325882
yanelena <fureevaalyona@mail.ru>
2021-04-26 23:48:09 +0300

    add BloomFilter

--- src/ADS/BloomFilter/BloomFilter.java
+++ src/ADS/BloomFilter/BloomFilter.java
@@ -1,40 +1,56 @@
 package ADS.BloomFilter;
 
-public class BloomFilter
-{
+public class BloomFilter {
     public int filter_len;
+    byte[] bits;
 
-    public BloomFilter(int f_len)
-    {
+    public BloomFilter(int f_len) {
         filter_len = f_len;
+        bits = new byte[filter_len];
         // создаём битовый массив длиной f_len ...
     }
 
     // хэш-функции
-    public int hash1(String str1)
-    {
+    public int hash1(String str1) {
         // 17
-        for(int i=0; i<str1.length(); i++)
-        {
-            int code = (int)str1.charAt(i);
+        int randomNum = 17;
+        int result = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            result *= randomNum;
+            int code = (int) str1.charAt(i);
+            result += code;
+            result %= filter_len;
+
         }
         // реализация ...
-        return 0;
-    }
-    public int hash2(String str1)
-    {
-        // 223
-        // реализация ...
-        return 0;
+        return result;
     }
 
-    public void add(String str1)
-    {
+    public int hash2(String str1) {
+        // 223
+        int randomNum = 223;
+        int result = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            result *= randomNum;
+            int code = (int) str1.charAt(i);
+            result += code;
+            result %= filter_len;
+
+        }
+        // реализация ...
+        return result;
+    }
+
+    public void add(String str1) {
+        bits[hash1(str1)] = 1;
+        bits[hash2(str1)] = 1;
         // добавляем строку str1 в фильтр
     }
 
-    public boolean isValue(String str1)
-    {
+    public boolean isValue(String str1) {
+        if (bits[hash1(str1)] == 1 && bits[hash2(str1)] == 1) {
+            return true;
+        }
         // проверка, имеется ли строка str1 в фильтре
         return false;
     }
